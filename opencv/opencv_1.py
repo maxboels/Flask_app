2c68eaf646b56c8faf58ae2c1d203af3580b3c2a
Maxence Boels <boelsmaxence@gmail.com>
2021-02-09 20:08:30 +0100

    Enter

--- opencv/opencv_1.py
+++ opencv/opencv_1.py
@@ -3,10 +3,10 @@ import cv2
 
 img = cv2.imread("hearth.jpg", 0)
 
-# BGR default
-cv2.imshow('Image', img)
-cv2.waitKey(0) #wait infinate amount of time with 0
-cv2.destroyAllWindows()
+# # BGR default
+# cv2.imshow('Image', img)
+# cv2.waitKey(0) #wait infinate amount of time with 0
+# cv2.destroyAllWindows()
 
 
 
