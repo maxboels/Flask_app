c347010d724f78fb9773aa6a0d4e61862860d712
Maxence Boels <boelsmaxence@gmail.com>
2021-02-09 20:03:50 +0100

    Enter

---
+++ opencv/opencv_1.py
@@ -0,0 +1,29 @@
+import cv2
+
+
+img = cv2.imread("hearth.jpg", 0)
+
+# BGR default
+cv2.imshow('Image', img)
+cv2.waitKey(0) #wait infinate amount of time with 0
+cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
